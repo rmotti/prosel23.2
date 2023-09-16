39beb0bdd439ef399b0abc1df946976ab8e42030
Rodrigo Motti <rmotti99@gmail.com>
2023-09-16 12:24:50 -0300

    Feat: innitial commit

---
+++ modal.js
@@ -0,0 +1,76 @@
+
+const btnOpen1 = document.getElementById('open1');
+const btnClose1 = document.getElementById('close1');
+const modal1 = document.getElementById('modal1');
+btnOpen1.addEventListener('click', openModal1);
+btnClose1.addEventListener('click', closeModal1);
+function openModal1() {
+    modal1.showModal();
+}
+function closeModal1() {
+    modal1.close();
+}
+
+const btnOpen2 = document.getElementById('open2');
+const btnClose2 = document.getElementById('close2');
+const modal2 = document.getElementById('modal2');
+btnOpen2.addEventListener('click', openModal2);
+btnClose2.addEventListener('click', closeModal2);
+function openModal2() {
+    modal2.showModal();
+}
+function closeModal2() {
+    modal2.close();
+}
+
+const btnOpen3 = document.getElementById('open3');
+const btnClose3 = document.getElementById('close3');
+const modal3 = document.getElementById('modal3');
+btnOpen3.addEventListener('click', openModal3);
+btnClose3.addEventListener('click', closeModal3);
+function openModal3() {
+    modal3.showModal();
+}
+function closeModal3() {
+    modal3.close();
+}
+
+const btnOpen4 = document.getElementById('open4');
+const btnClose4 = document.getElementById('close4');
+const modal4 = document.getElementById('modal4');
+btnOpen4.addEventListener('click', openModal4);
+btnClose4.addEventListener('click', closeModal4);
+function openModal4() {
+    modal4.showModal();
+}
+function closeModal4() {
+    modal4.close();
+}
+
+const btnOpen5 = document.getElementById('open5');
+const btnClose5 = document.getElementById('close5');
+const modal5 = document.getElementById('modal5');
+btnOpen5.addEventListener('click', openModal5);
+btnClose5.addEventListener('click', closeModal5);
+function openModal5() {
+    modal5.showModal();
+}
+function closeModal5() {
+    modal5.close();
+}
+
+const btnOpen6 = document.getElementById('open6');
+const btnClose6 = document.getElementById('close6');
+const modal6 = document.getElementById('modal6');
+btnOpen6.addEventListener('click', openModal6);
+btnClose6.addEventListener('click', closeModal6);
+function openModal6() {
+    modal6.showModal();
+}
+function closeModal6() {
+    modal6.close();
+}
+
+
+
+
